93f1feebe478d5357fdfd4ba204c571960dc3cb4
davilux <103956666+davilux@users.noreply.github.com>
2023-05-25 17:30:07 -0600

    Sparkles load and respond correctly to a reduce motion accessibility setting. However, they seem to be constrained to a small corner of the element they are applied to.

--- src/sparkles/Sparkles.js
+++ src/sparkles/Sparkles.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 
-//Created following a tutorial by Josh Comeau: https://www.joshwcomeau.com/react/animated-sparkles-in-react/
+/*-------- ABOUT --------*/
+//Created with guidance from Josh Comeau: https://www.joshwcomeau.com/react/animated-sparkles-in-react/
 
-//Usage:
+/*-------- USAGE --------*/
 //  It can be used on images:
 // <Sparkles>
 //   <img src="teacup.png" alt="A teacup filled with bubbly lavender tea." />
@@ -13,8 +14,74 @@ import styled, { keyframes } from "styled-components";
 //   The phrase all devs know, <Sparkles>Hello World</Sparkles>.
 // </p>
 
+/*-------- HELPER FUNCTIONS --------*/
+
 // Utility helper for random number generation
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+//Helper function used to generate an array
+const range = (start, end, step = 1) => {
+  let output = [];
+  if (typeof end === "undefined") {
+    end = start;
+    start = 0;
+  }
+  for (let i = start; i < end; i += step) {
+    output.push(i);
+  }
+  return output;
+};
+
+/*-------- CUSTOM HOOKS --------*/
+
+//Custom hook that works like setInterval, except you pass it two numbers, a min and a max. For each iteration, it picks a random number in that range.
+const useRandomInterval = (callback, minDelay, maxDelay) => {
+  const timeoutId = React.useRef(null);
+  const savedCallback = React.useRef(callback);
+  React.useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+  React.useEffect(() => {
+    let isEnabled =
+      typeof minDelay === "number" && typeof maxDelay === "number";
+    if (isEnabled) {
+      const handleTick = () => {
+        const nextTickAt = random(minDelay, maxDelay);
+        timeoutId.current = window.setTimeout(() => {
+          savedCallback.current();
+          handleTick();
+        }, nextTickAt);
+      };
+      handleTick();
+    }
+    return () => window.clearTimeout(timeoutId.current);
+  }, [minDelay, maxDelay]);
+  const cancel = React.useCallback(function () {
+    window.clearTimeout(timeoutId.current);
+  }, []);
+  return cancel;
+};
+
+//Custom hook. The "prefers reduced motion" media query allows people to indicate that they don't want to see any animations. The usePrefersReducedMotion hook lets us access that value from within JS.
+const QUERY = "(prefers-reduced-motion: no-preference)";
+const getInitialState = () => !window.matchMedia(QUERY).matches;
+function usePrefersReducedMotion() {
+  const [prefersReducedMotion, setPrefersReducedMotion] =
+    React.useState(getInitialState);
+  React.useEffect(() => {
+    const mediaQueryList = window.matchMedia(QUERY);
+    const listener = (event) => {
+      setPrefersReducedMotion(!event.matches);
+    };
+    mediaQueryList.addEventListener("change", listener);
+    return () => {
+      mediaQueryList.removeEventListener("change", listener);
+    };
+  }, []);
+  return prefersReducedMotion;
+}
+
+/*-------- MAIN FUNCTIONALITY --------*/
 
 //generateSparkle creates the data for a new sparkle instance.
 const DEFAULT_COLOR = "#FFFFFFFF";
@@ -37,22 +104,61 @@ const generateSparkle = (color = DEFAULT_COLOR) => {
   return sparkle;
 };
 
-//react component that returns a single sparkle SVG
+//React component that returns a single sparkle SVG
 function SparkleInstance({ color, size, style }) {
   const path =
     "M125 0C125 0 127.551 67.4468 155.052 94.948C182.553 122.449 250 125 250 125C250 125 182.553 127.551 155.052 155.052C127.551 182.553 125 250 125 250C125 250 122.449 182.553 94.948 155.052C67.4468 127.551 0 125 0 125C0 125 67.4468 122.449 94.948 94.948C122.449 67.4468 125 0 125 0Z";
   return (
-    <SparkleSvg
-      width={size}
-      height={size}
-      viewBox="0 0 250 250"
-      fill="none"
-      style={style}
-    >
-      <path d={path} fill={color} />
-    </SparkleSvg>
+    <SparkleWrapper>
+      <SparkleSvg
+        width={size}
+        height={size}
+        viewBox="0 0 250 250"
+        fill="none"
+        style={style}
+      >
+        <path d={path} fill={color} />
+      </SparkleSvg>
+    </SparkleWrapper>
   );
 }
+
+//React component to render all sparkles!
+function Sparkles({ color = DEFAULT_COLOR, children }) {
+  const [sparkles, setSparkles] = React.useState(() => {
+    return range(4).map(() => generateSparkle(color));
+  });
+  const prefersReducedMotion = usePrefersReducedMotion();
+  useRandomInterval(
+    () => {
+      const sparkle = generateSparkle(color);
+      const now = Date.now();
+      const nextSparkles = sparkles.filter((sp) => {
+        const delta = now - sp.createdAt;
+        return delta < 750;
+      });
+      nextSparkles.push(sparkle);
+      setSparkles(nextSparkles);
+    },
+    prefersReducedMotion ? null : 50,
+    prefersReducedMotion ? null : 450
+  );
+
+  return (
+    <Wrapper>
+      {sparkles.map((sparkle) => (
+        <SparkleInstance
+          color={sparkle.color}
+          size={sparkle.size}
+          style={sparkle.style}
+        />
+      ))}
+      <ChildWrapper>{children}</ChildWrapper>
+    </Wrapper>
+  );
+}
+
+/*-------- STYLED COMPONENTS --------*/
 
 const comeInOut = keyframes`
   0% {
@@ -66,6 +172,11 @@ const comeInOut = keyframes`
   }
 `;
 
+const SparkleWrapper = styled.span`
+  position: absolute;
+  display: block;
+`;
+
 const SparkleSvg = styled.svg`
   display: block;
   @media (prefers-reduced-motion: no-preference) {
@@ -73,20 +184,6 @@ const SparkleSvg = styled.svg`
   }
 `;
 
-//React component to render all sparkles!
-function Sparkles({ children }) {
-  const sparkle = generateSparkle();
-  return (
-    <Wrapper>
-      <SparkleInstance
-        color={sparkle.color}
-        size={sparkle.size}
-        style={sparkle.style}
-      />
-      <ChildWrapper>{children}</ChildWrapper>
-    </Wrapper>
-  );
-}
 const Wrapper = styled.span`
   position: relative;
   display: inline-block;
